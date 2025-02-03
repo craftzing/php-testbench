9822849c20283521d991391803ed0019910e81c8
ppelgrims <dev@pieterpelgrims.com>
2025-02-03 16:25:43 +0100

    Rename php cs fixer config file

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,42 +1,56 @@
 <?php
 
-$finder = Symfony\Component\Finder\Finder::create()
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+use Symfony\Component\Finder\Finder;
+
+$finder = Finder::create()
     ->in([
         __DIR__ . '/src',
-        __DIR__ . '/config',
+        __DIR__ . '/tests',
     ])
     ->name('*.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return PhpCsFixer\Config::create()
+return (new Config())
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setRiskyAllowed(true)
     ->setRules([
-        '@PSR2' => true,
+        '@PHP84Migration' => true,
         'array_syntax' => ['syntax' => 'short'],
         'ordered_imports' => [
             'imports_order' => ['class', 'function', 'const'],
-            'sortAlgorithm' => 'alpha',
+            'sort_algorithm' => 'alpha',
+        ],
+        'fully_qualified_strict_types' => [
+            'phpdoc_tags' => [],
         ],
         'no_unused_imports' => true,
+        'blank_line_after_opening_tag' => true,
+        'declare_strict_types' => true,
         'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline' => [
+            'elements' => ['arguments', 'arrays', 'match', 'parameters'],
+        ],
         'phpdoc_scalar' => true,
         'unary_operator_spaces' => true,
         'binary_operator_spaces' => true,
         'blank_line_before_statement' => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
+        'no_extra_blank_lines' => [
+            'tokens' => ['parenthesis_brace_block', 'return', 'square_brace_block', 'extra'],
+        ],
         'phpdoc_single_line_var_spacing' => true,
         'phpdoc_var_without_name' => true,
-        'class_attributes_separation' => [
-            'elements' => [
-                'method',
-            ],
-        ],
         'method_argument_space' => [
             'on_multiline' => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
         ],
         'void_return' => true,
+        'single_quote' => true,
     ])
     ->setFinder($finder);
