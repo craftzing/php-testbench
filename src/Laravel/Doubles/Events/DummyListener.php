4f6a07d42657638b742127216b35a3200d45ecf2
Rémi Pelhate <remi.pelhate@icloud.com>
2025-06-02 15:01:17 +0200

    Exclude test doubled from test coverage

--- src/Laravel/Doubles/Events/DummyListener.php
+++ src/Laravel/Doubles/Events/DummyListener.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Craftzing\TestBench\Laravel\Doubles\Events;
 
+/**
+ * @codeCoverageIgnore
+ */
 final readonly class DummyListener
 {
     public function __invoke(mixed ...$arguments): void
