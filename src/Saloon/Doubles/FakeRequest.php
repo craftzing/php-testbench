169c39d70d0ae8e1283ef0ca1ccf3c95411f7615
Rémi Pelhate <remi.pelhate@icloud.com>
2025-06-10 16:47:30 +0200

    Add support for DTOs in FakeRequest

--- src/Saloon/Doubles/FakeRequest.php
+++ src/Saloon/Doubles/FakeRequest.php
@@ -4,18 +4,27 @@ declare(strict_types=1);
 
 namespace Craftzing\TestBench\Saloon\Doubles;
 
+use Override;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
+use Saloon\Http\Response;
 
 final class FakeRequest extends Request
 {
     public function __construct(
         protected Method $method = Method::GET,
         public readonly string $endpoint = '/fake',
+        public readonly mixed $dto = null,
     ) {}
 
     public function resolveEndpoint(): string
     {
         return $this->endpoint;
+    }
+
+    #[Override]
+    public function createDtoFromResponse(Response $response): mixed
+    {
+        return $this->dto;
     }
 }
