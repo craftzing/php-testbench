4df68403b1481ecfd37d4e5f2f2840fba59ad5e1
Rémi Pelhate <remi.pelhate@icloud.com>
2025-06-10 16:34:56 +0200

    Add withAuthentication() to FakeConnector

--- src/Saloon/Doubles/FakeConnector.php
+++ src/Saloon/Doubles/FakeConnector.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace Craftzing\TestBench\Saloon\Doubles;
 
+use Saloon\Http\Auth\NullAuthenticator;
 use Saloon\Http\Connector;
 use Saloon\Traits\HasMockClient;
 
 final class FakeConnector extends Connector
 {
     use HasMockClient;
+
+    public function withAuthentication(): self
+    {
+        return new self()->authenticate(new NullAuthenticator());
+    }
 
     public function resolveBaseUrl(): string
     {
