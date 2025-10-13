35c87e7a25cfefa6e0d6f1d2f5ea2c9de35783c6
ppelgrims <pieter.pelgrims@craftzing.com>
2025-10-13 21:04:18 +0200

    Make class pass phpstan

--- src/PHPUnit/DataProviders/EnumCase.php
+++ src/PHPUnit/DataProviders/EnumCase.php
@@ -21,17 +21,18 @@ use function in_array;
  */
 final readonly class EnumCase
 {
-    /**
-     * @var array<int, TValue>
-     */
+    /** @var TValue */
+    public UnitEnum $instance;
+
+    /** @var array<int, TValue> */
     private array $options;
 
-    public function __construct(
-        /* @var TValue */
-        public UnitEnum $instance,
-        /* @param array<int, TValue> ...$options */
-        UnitEnum ...$options,
-    ) {
+    /**
+     * @param TValue $instance
+     * @param TValue ...$options
+     */
+    public function __construct(UnitEnum $instance, UnitEnum ...$options)
+    {
         in_array($instance, $options, true) or throw new ValueError('Options should contain the given instance.');
 
         foreach ($options as $option) {
@@ -40,6 +41,7 @@ final readonly class EnumCase
             );
         }
 
+        $this->instance = $instance;
         $this->options = $options;
     }
 
@@ -51,51 +53,71 @@ final readonly class EnumCase
         count($this->options) > 1 or throw new LogicException(
             self::class . ' was configured with a single option and can therefore not return a different instance.',
         );
-        $differentOptions = array_filter($this->options, fn (UnitEnum $option): bool => $option !== $this->instance);
+
+        $differentOptions = array_filter(
+            $this->options,
+            fn (UnitEnum $option): bool => $option !== $this->instance,
+        );
 
         return $differentOptions[array_rand($differentOptions)];
     }
 
     /**
      * @param class-string<TValue> $enumFQCN
-     * @return iterable<array<self<TValue>>>
+     * @return iterable<array{self<TValue>}>
      */
     public static function cases(string $enumFQCN): iterable
     {
+        /** @var ReflectionEnumUnitCase $case */
         foreach (new ReflectionEnum($enumFQCN)->getCases() as $case) {
-            // @phpstan-ignore generator.valueType
-            yield "$enumFQCN::$case->name" => [
-                new self($case->getValue(), ...$enumFQCN::cases()),
-            ];
+            /** @var TValue $value */
+            $value = $case->getValue();
+            /** @var array<int, TValue> $all */
+            $all = $enumFQCN::cases();
+            /** @var self<TValue> $enumCase */
+            $enumCase = new self($value, ...$all);
+
+            yield "$enumFQCN::{$case->name}" => [$enumCase];
         }
     }
 
     /**
      * @param TValue ...$options
-     * @return iterable<array<self<TValue>>>
+     * @return iterable<array{self<TValue>}>
      */
     public static function options(UnitEnum ...$options): iterable
     {
+        /** @var array<int, TValue> $options */
         foreach ($options as $case) {
-            yield "$case->name" => [new self($case, ...$options)];
+            /** @var TValue $case */
+            /** @var self<TValue> $enumCase */
+            $enumCase = new self($case, ...$options);
+            yield "{$case->name}" => [$enumCase];
         }
     }
 
     /**
      * @param class-string<TValue> $enumFQCN
-     * @return iterable<array<self<TValue>>>
+     * @param TValue ...$except
+     * @return iterable<array{self<TValue>}>
      */
     public static function except(string $enumFQCN, UnitEnum ...$except): iterable
     {
-        $options = array_map(function (ReflectionEnumUnitCase $reflection) use ($except): ?UnitEnum {
-            $case = $reflection->getValue();
+        /** @var class-string<TValue> $enumFQCN */
+        /** @var array<int, TValue> $except */
+        $options = array_map(
+            function (ReflectionEnumUnitCase $reflection) use ($except): ?UnitEnum {
+                $case = $reflection->getValue();
 
-            return match (in_array($case, $except, true)) {
-                true => null,
-                false => $case,
-            };
-        }, new ReflectionEnum($enumFQCN)->getCases());
+                return in_array($case, $except, true) ? null : $case;
+            },
+            new ReflectionEnum($enumFQCN)->getCases(),
+        );
 
-        yield from self::options(...array_filter($options));
+        /** @var array<int, TValue|null> $options */
+        /** @var array<int, TValue> $filtered */
+        $filtered = array_filter($options);
+
+        yield from self::options(...$filtered);
     }
 }
