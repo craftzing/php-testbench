2a94591f8910407bd2cfd947428c3bb73a78c61f
Rémi Pelhate <remi.pelhate@icloud.com>
2026-06-04 11:18:53 +0200

    Mago fix no-empty-comment

--- src/Laravel/Doubles/Events/DummyListener.php
+++ src/Laravel/Doubles/Events/DummyListener.php
@@ -9,13 +9,6 @@ namespace Craftzing\TestBench\Laravel\Doubles\Events;
  */
 final readonly class DummyListener
 {
-    public function __invoke(mixed ...$arguments): void
-    {
-        //
-    }
-
-    public function handle(): void
-    {
-        //
-    }
+    public function __invoke(mixed ...$arguments): void {}
+    public function handle(): void {}
 }
