a020a5b3baf933cc129bab93f95d5138bf585c87
Rémi Pelhate <remi.pelhate@icloud.com>
2026-06-04 10:43:44 +0200

    Mago fix prefer-arrow-function

--- src/PHPUnit/Constraint/Objects/DeriveConstraintsFromObjectUsingReflection.php
+++ src/PHPUnit/Constraint/Objects/DeriveConstraintsFromObjectUsingReflection.php
@@ -18,8 +18,8 @@ final readonly class DeriveConstraintsFromObjectUsingReflection
      */
     public function __invoke(object $object): array
     {
-        return array_map(static function (ReflectionProperty $property) use ($object): Constraint {
-            return new PropertyValue($property->name, new IsEqual($property->getValue($object)));
-        }, new ReflectionClass($object)->getProperties(ReflectionProperty::IS_PUBLIC));
+        return array_map(static fn (ReflectionProperty $property): PropertyValue => (
+            new PropertyValue($property->name, new IsEqual($property->getValue($object)))
+        ), new ReflectionClass($object)->getProperties(ReflectionProperty::IS_PUBLIC));
     }
 }
