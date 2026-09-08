922b2bb7da7ddd580ed6d507cf6db857eaa2547b
Rémi Pelhate <remi.pelhate@icloud.com>
2026-09-08 07:36:24 +0200

    Fix kan-defect on HttpStatusCode data provider

--- src/PHPUnit/DataProviders/HttpStatusCode.php
+++ src/PHPUnit/DataProviders/HttpStatusCode.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 final readonly class HttpStatusCode
 {
+    private const int MAX_CODE = 600;
+
     public function __construct(
         public int $code,
         public string $message,
@@ -19,90 +21,56 @@ final readonly class HttpStatusCode
     }
 
     /** @return iterable<list<self>> */
-    public static function all(): iterable
+    private static function generate(int $includedMinCode, int $excludedMaxCode): iterable
     {
         foreach (Response::$statusTexts as $statusCode => $message) {
+            if ($statusCode < $includedMinCode) {
+                continue;
+            }
+
+            if ($statusCode >= $excludedMaxCode) {
+                continue;
+            }
+
             yield "{$statusCode} {$message}" => [
                 new self($statusCode, $message),
             ];
         }
+    }
+
+    /** @return iterable<list<self>> */
+    public static function all(): iterable
+    {
+        return self::generate(Response::HTTP_CONTINUE, self::MAX_CODE);
     }
 
     /** @return iterable<list<self>> */
     public static function errors(): iterable
     {
-        foreach (Response::$statusTexts as $statusCode => $message) {
-            if ($statusCode < Response::HTTP_BAD_REQUEST) {
-                continue;
-            }
-
-            yield "{$statusCode} {$message}" => [
-                new self($statusCode, $message),
-            ];
-        }
+        return self::generate(Response::HTTP_BAD_REQUEST, self::MAX_CODE);
     }
 
     /** @return iterable<list<self>> */
     public static function success(): iterable
     {
-        foreach (Response::$statusTexts as $statusCode => $message) {
-            if ($statusCode >= Response::HTTP_MULTIPLE_CHOICES) {
-                continue;
-            }
-
-            yield "{$statusCode} {$message}" => [
-                new self($statusCode, $message),
-            ];
-        }
+        return self::generate(Response::HTTP_CONTINUE, Response::HTTP_MULTIPLE_CHOICES);
     }
 
     /** @return iterable<list<self>> */
     public static function redirection(): iterable
     {
-        foreach (Response::$statusTexts as $statusCode => $message) {
-            if ($statusCode < Response::HTTP_MULTIPLE_CHOICES) {
-                continue;
-            }
-
-            if ($statusCode >= Response::HTTP_BAD_REQUEST) {
-                continue;
-            }
-
-            yield "{$statusCode} {$message}" => [
-                new self($statusCode, $message),
-            ];
-        }
+        return self::generate(Response::HTTP_MULTIPLE_CHOICES, Response::HTTP_BAD_REQUEST);
     }
 
     /** @return iterable<list<self>> */
     public static function clientErrors(): iterable
     {
-        foreach (Response::$statusTexts as $statusCode => $message) {
-            if ($statusCode < Response::HTTP_BAD_REQUEST) {
-                continue;
-            }
-
-            if ($statusCode >= Response::HTTP_INTERNAL_SERVER_ERROR) {
-                continue;
-            }
-
-            yield "{$statusCode} {$message}" => [
-                new self($statusCode, $message),
-            ];
-        }
+        return self::generate(Response::HTTP_BAD_REQUEST, Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 
     /** @return iterable<list<self>> */
     public static function serverErrors(): iterable
     {
-        foreach (Response::$statusTexts as $statusCode => $message) {
-            if ($statusCode < Response::HTTP_INTERNAL_SERVER_ERROR) {
-                continue;
-            }
-
-            yield "{$statusCode} {$message}" => [
-                new self($statusCode, $message),
-            ];
-        }
+        return self::generate(Response::HTTP_INTERNAL_SERVER_ERROR, self::MAX_CODE);
     }
 }
